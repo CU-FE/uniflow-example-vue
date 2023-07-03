243d90861c838a2a62cc6093d2594f176e6eb87e
lizhijie1 <lizhijie1@360.cn>
2023-07-03 17:24:20 +0800

    uniflow vue示例

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import EasyFlow from "./EasyFlow";
+import { ref } from "vue";
+const data = ref({
+  mediav: {
+    showid: "XsgsZg",
+  },
+  mlist: {
+    sign: "look",
+  },
+  uid: "234898968.541588155933378800.1673835122257.8027",
+});
+</script>
+
+<template>
+  <div>EasyFlow</div>
+  <EasyFlow :config="data"></EasyFlow>
+</template>
